f345ec7e524c28165ac63d836f32e57a68fc921f
Ram Aditya Nandula <nandulram@gmail.com>
2021-06-17 21:58:52 -0500

    Added files

---
+++ script.js
@@ -0,0 +1,16 @@
+// Ram Aditya Nandula - 6/17/21
+// Odin Project - Library
+
+let myLibrary = [];
+
+function Book() {
+  // the constructor...
+}
+
+function addBookToLibrary() {
+  // do stuff here
+  let title = prompt("Enter the book title: ")
+  let author = prompt("Enter the book author: ")
+  let numPages = prompt("Enter the book pages: ")
+  let hasBeenRead = prompt("Enter whether the book has been read: ")
+}
