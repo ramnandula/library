3da6164be9cc90d6226326d1c0dd35bedc2b8217
Ram Aditya Nandula <nandulram@gmail.com>
2021-06-24 21:55:22 -0500

    Started form

--- script.js
+++ script.js
@@ -1,16 +1,40 @@
-// Ram Aditya Nandula - 6/17/21
+// Ram Aditya Nandula - 6/24/21
 // Odin Project - Library
 
 let myLibrary = [];
 
-function Book() {
-  // the constructor...
+function Book(title, author, pages, hasBeenRead) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.hasBeenRead = hasBeenRead
 }
 
-function addBookToLibrary() {
-  // do stuff here
-  let title = prompt("Enter the book title: ")
-  let author = prompt("Enter the book author: ")
-  let numPages = prompt("Enter the book pages: ")
-  let hasBeenRead = prompt("Enter whether the book has been read: ")
+function displayBooks() {
+
 }
+
+window.onload = function () {
+//     let title = prompt("Enter the book title: ")
+//     let author = prompt("Enter the book author: ")
+//     let numPages = prompt("Enter the book pages: ")
+//     let hasBeenRead = prompt("Enter whether the book has been read: ")
+
+//     const book = new Book(title, author, numPages, hasBeenRead)
+//     myLibrary.push(book)
+    closeForm();
+}
+
+const btn = document.querySelector('#btn');
+btn.addEventListener('click', () => {
+    // alert("Hello World!");
+    openForm();
+});
+
+function openForm() {
+    document.querySelector('#myForm').style.display = "block";
+}
+  
+function closeForm() {
+    document.querySelector('#myForm').style.display = "none";
+} 
