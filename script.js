4b712a207f24f31a4bb6cc18eacd8a7cd29a0d1c
Ram Aditya Nandula <nandulram@gmail.com>
2021-07-27 12:36:07 -0500

    Added local storage functionality

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-// Ram Aditya Nandula - 6/24/21
+// Ram Aditya Nandula - 7/19/21
 // Odin Project - Library
 
-let myLibrary = [];
+let myLibrary = []; // array of books
 
+// object constructor
 function Book(title, author, pages, hasBeenRead) {
     this.title = title;
     this.author = author;
@@ -10,24 +11,30 @@ function Book(title, author, pages, hasBeenRead) {
     this.hasBeenRead = hasBeenRead;
 }
 
+// creates and stores book object into library
 function addBookToLibrary(title, author, numPages, hasBeenRead) {
     const book = new Book(title, author, numPages, hasBeenRead);
     myLibrary.push(book);
-    console.log(myLibrary)
-    console.log("added book");
 }
 
 function removeBookFromLibrary() {
 }
 
+// saves the whole library array to localStorage
 function saveLibrary() {
     localStorage.setItem('library', JSON.stringify(myLibrary));
 }
 
+// looks for library array in localStorage
 function retrieveLibrary() {
-    const library = localStorage.getItem('library');
+    // if (localStorage.getItem('library')) {
+        const library = localStorage.getItem('library');
+        myLibrary = JSON.parse(library);
+    // }
+    displayLibrary();
 }
 
+// clears page 
 function clearTable() {
     const table = document.querySelector('#library');
     while (table.rows.length > 0) {
@@ -48,12 +55,12 @@ function displayLibrary() {
         var cell4 = row.insertCell(3);
         var cell5 = row.insertCell(4);
         var cell6 = row.insertCell(5);
-        var cell7 = row.insertCell(6);
 
+        // displays book info on table
         cell1.innerHTML = book.title;
         cell2.innerHTML = book.author;
-        cell3.innerHTML = book.pages;
-        cell4.innerHTML = book.hasBeenRead;
+        cell3.innerHTML = book.pages + " pages";
+        cell4.innerHTML = book.hasBeenRead ? "Been read" : "Not been read";
         
         const changeBtn = document.createElement('button');
         changeBtn.innerHTML = "Change Read Status";
@@ -64,14 +71,16 @@ function displayLibrary() {
             const book = myLibrary[index];
 
             if (book.hasBeenRead) {
-                book.hasBeenRead = "false";
+                book.hasBeenRead = false;
             }
             else {
                 book.hasBeenRead = true;
             }
+            saveLibrary();
             displayLibrary();
         });
        
+        // removes the book from the library
         const removeBtn = document.createElement('button');
         removeBtn.innerHTML = "Remove Book";
         cell6.appendChild(removeBtn);
@@ -79,12 +88,13 @@ function displayLibrary() {
             const index = removeBtn.parentNode.parentNode.rowIndex;
             console.log(index);
             myLibrary.splice(index, 1);
+            saveLibrary();
             displayLibrary();
         });
     }
-    console.log("displayed book");
 }
 
+// brings up a form allowing users to input the details for the new book
 const newBtn = document.querySelector('.new-button');
 newBtn.addEventListener('click', () => {
     openForm();
@@ -97,12 +107,16 @@ addBtn.addEventListener('click', function (e) {
     const author = document.querySelector('#author').value;
     const numPages = document.querySelector('#pages').value;
     const hasBeenRead = document.querySelector('#read').checked;
+    
+    if (title.trim().length != 0 && author.trim().length != 0 && numPages.length != 0) {
+        addBookToLibrary(title, author, numPages, hasBeenRead);
+        saveLibrary();
+        displayLibrary();
+    }
 
-    addBookToLibrary(title, author, numPages, hasBeenRead);
-    saveLibrary();
-    displayLibrary();
 });
 
+// closes the form
 const closeBtn = document.querySelector('.cancel-button');
 closeBtn.addEventListener('click', function (e) {
     e.preventDefault();
@@ -116,3 +130,5 @@ function openForm() {
 function closeForm() {
     document.querySelector('#myForm').style.display = "none";
 }
+
+retrieveLibrary();
